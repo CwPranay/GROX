30c14f41b068f8c4eb555c2f3083c66acd772c4a
Pranay Gurav <guravpranay81@gmail.com>
2026-04-08 13:50:33 +0530

    chanegd email

--- grox-web/api/request.js
+++ grox-web/api/request.js
@@ -24,8 +24,8 @@ export default async function handler(req, res) {
         }
 
         const response = await resend.emails.send({
-            from: "GROX <hello@claroupkel.resend.app>",
-            to: "groxindia.business@gmail.com",
+            from: "onboarding@resend.dev",
+            to: "prngurav@gmail.com",
             subject: "New GROX Request",
             html: `<h2>New Request</h2>
 
